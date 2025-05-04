d721003b90003b33c3ef5b1e1994707af147b596
saurabh <jainsaurabh693@gmail.com>
2025-05-04 10:30:59 +0530

    "connection to firestore"

---
+++ app/src/main/java/com/example/mytravelbuddy/Models/User.java
@@ -0,0 +1,43 @@
+package com.example.mytravelbuddy.Models;
+
+public class User {
+    String id;
+    String name;
+    String password;
+    String email;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
